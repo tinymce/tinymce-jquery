650edc537f6cd9d55c475c55cf31a823220c7d3a
James Johnson <james.johnson@tiny.cloud>
2023-03-27 12:54:38 +1000

    INT-3112: Readded storybook

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/html",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  "webpackFinal": async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.m?js$/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
+    // Return the altered config
+    return config;
+  }
+}
