dc0be10e88b69f29e08659f64f6e8715545ad949
Ben Tran <ben.tran@tiny.cloud>
2026-05-21 15:15:55 +0930

    Remove all try catches

--- src/test/ts/Utils.ts
+++ src/test/ts/Utils.ts
@@ -7,25 +7,21 @@ export const createEditor = async (action: (targetElm: JQuery<HTMLElement>, edit
   // TinyMCE must be in the document to work
   const ce = SugarElement.fromTag('textarea');
   Insert.append(SugarBody.body(), ce);
-  try {
-    const targetElm = $(ce.dom);
-    const editors = await targetElm.tinymce({
-      license_key: 'gpl',
-      script_url: '/project/node_modules/tinymce/tinymce.js',
-    });
+  const targetElm = $(ce.dom);
+  const editors = await targetElm.tinymce({
+    license_key: 'gpl',
+    script_url: '/project/node_modules/tinymce/tinymce.js',
+  });
 
-    try {
-      const maybeAsync = action(targetElm, editors[0]);
-      if (maybeAsync) {
-        await maybeAsync;
-      }
-    } finally {
-      editors[0].remove();
-      await Waiter.pTryUntilPredicate('Editor should be removed', () => $(ce.dom).tinymce() === undefined);
-    }
-  } finally {
-    Remove.remove(ce);
+  await Waiter.pTryUntil('Editor should be initialized', () => editors[0].initialized);
+
+  const maybeAsync = action(targetElm, editors[0]);
+  if (maybeAsync) {
+    await maybeAsync;
   }
+  editors[0].remove();
+  await Waiter.pTryUntilPredicate('Editor should be removed', () => $(ce.dom).tinymce() === undefined);
+  Remove.remove(ce);
 };
 
 export const createHTML = async (html: string, action: (root: HTMLElement) => void | Promise<void>) => {
